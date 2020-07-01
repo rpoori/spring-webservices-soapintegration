e76ce92febbee2980b2eb6bd25c6921300460da5
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-07-01 07:45:33 -0400

    Spring boot app domain driven design skeleton setup

---
+++ apis/sample-api/src/main/java/com/my/poc/springbootdomaindrivendesign/SpringBootDomainDrivenDesignApplication.java
@@ -0,0 +1,13 @@
+package com.my.poc.springbootdomaindrivendesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDomainDrivenDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDomainDrivenDesignApplication.class, args);
+	}
+
+}
